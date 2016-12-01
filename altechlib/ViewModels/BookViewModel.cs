0eac9986bca3067ef4994b750f937ebc54caaa84
manag_000 <andrei.lavrinovic@gmail.com>
2016-12-01 21:04:23 +0000

    altechlib(v_0.0.1) => ViewModels.BookViewModel

---
+++ altechlib/ViewModels/BookViewModel.cs
@@ -0,0 +1,39 @@
+﻿using altechlib.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace altechlib.ViewModels
+{
+    class BookViewModel : NotificationBase<Book>
+    {
+        // constructor
+        public BookViewModel(Book book = null) : base(book) { }
+
+        public String Isbn
+        {
+            get { return this.Isbn; }
+            set { SetProperty(This.isbn, value, () => This.isbn = value); }
+        }
+
+        public String Name
+        {
+            get { return this.Name; }
+            set { SetProperty(This.name, value, () => This.name = value); }
+        }
+
+        public String Author
+        {
+            get { return this.Author; }
+            set { SetProperty(This.author, value, () => This.author = value); }
+        }
+
+        public String Content
+        {
+            get { return this.Content; }
+            set { SetProperty(This.content, value, () => This.content = value); }
+        }
+    }
+}
