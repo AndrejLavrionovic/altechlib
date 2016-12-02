f9de70ac7fdf30fa3164828af0b588eb2ff2bb00
manag_000 <andrei.lavrinovic@gmail.com>
2016-12-02 19:45:19 +0000

    altechlib(v_1.0.0) => myBooks.txt - db simulation

--- altechlib/Data/Book.cs
+++ altechlib/Data/Book.cs
@@ -7,35 +7,10 @@ namespace altechlib.Data
     public class Book
     {
         public string isbn { get; set; }
-        public string name { get; set; }
+        public string title { get; set; }
         public string author { get; set; }
         public string content { get; set; }
-    }
-
-    public class FakeService
-    {
-        public static String Name = "Fake Data Service.";
-
-        public static List<Book> GetBooks()
-        {
-            Debug.WriteLine("GET for books.");
-            return new List<Book>()
-                {
-                    new Book() { isbn="100001", name="Book #1", author = "Andrej Lavrinovic", content = "BlaBlaBlablaBafvoerfojewoirngvernejnfiuerho" },
-                    new Book() { isbn="100002", name="Book #2", author = "Tatjana Buta", content = "hbvnkjhsbc;ojbjenfbwiejfpoewjngvhjewnjfpowekjigubfjowiehgiuerjhf" },
-                    new Book() { isbn="100003", name="Book #3", author = "Aleksej Belousov", content = "ljahnvrgfnbvjlhsncoefdnblrnfwefjuiwrhguiejfivriutg feiojfh ieurhfe jgfreg" }
-                };
-        }
-
-        public static void Write(Book book)
-        {
-            Debug.WriteLine("INSERT book with name " + book.name);
-        }
-
-        public static void Delete(Book book)
-        {
-            Debug.WriteLine("DELETE book with name " + book.name);
-        }
-
+        public int favorite { get; set; }
+        public int img { get; set; }
     }
 }
