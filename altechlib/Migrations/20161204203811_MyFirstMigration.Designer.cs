4d113e7f03a86aa524abc742b2d73054d9353c37
manag_000 <andrei.lavrinovic@gmail.com>
2016-12-04 20:50:22 +0000

    altechlib(v_1.1.3) => commit before changing db

---
+++ altechlib/Migrations/20161204203811_MyFirstMigration.Designer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using altechlib.Data;
+
+namespace altechlib.Migrations
+{
+    [DbContext(typeof(LibraryContext))]
+    [Migration("20161204203811_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("altechlib.Data.Book", b =>
+                {
+                    b.Property<int>("BookId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Author");
+
+                    b.Property<string>("Content");
+
+                    b.Property<int>("Favorite");
+
+                    b.Property<string>("Genre");
+
+                    b.Property<string>("Image");
+
+                    b.Property<string>("Isbn");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("BookId");
+
+                    b.ToTable("Books");
+                });
+        }
+    }
+}
---
+++ altechlib/Migrations/20161204203811_MyFirstMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace altechlib.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Favorite = table.Column<int>(nullable: false),
+                    Genre = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Isbn = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
